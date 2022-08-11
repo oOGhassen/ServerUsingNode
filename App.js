643af9f9175511bc7f1615a24b2e507f4f491e6b
oOGhassen <nablighassen11@gmail.com>
2022-08-11 13:14:53 +0100

    node server firstTime

---
+++ App.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const port = 3000
+
+const server =http.createServer(function(req, res){
+
+})
+server.listen(port,function(error) {
+   if(error){
+console.log('something went wrong', error)
+   }else{
+      console.log('server is running on port '+port)
+   }
+})
